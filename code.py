c41110c2a9a9c07510ab19a64af217260b9914b0
ONKJUNUA <jaseko2002@gmail.com>
2022-06-18 14:28:44 +0200

    setting up the info tab

--- code.py
+++ code.py
@@ -1,10 +1,21 @@
 import PySimpleGUI as sg
 
-info_tab = [[]]
+sg.theme('DarkBlack')
+
+info_tab = [
+    [sg.Text('Title:'),sg.Text('',key = '-TITLE-')],
+    [sg.Text('Length:'),sg.Text('',key = '-LENGTH-')],
+    [sg.Text('Views:'),sg.Text('',key = '-VIEWS-')],
+    [sg.Text('Author:'),sg.Text('',key = '-AUTHOR-')],
+    [
+        sg.Text('Description:'),
+        sg.Multiline('', key = '-DESCRIPTION-', size = (40,20), no_scrollbar = True, disabled = True)
+    ]
+]
 download_tab = [[]]
 
 layout = [[sg.TabGroup([[
-    sg.Tab('info',info_tab),sg.Tab('download',download_tab)]])]]
+    sg.Tab('INFO',info_tab),sg.Tab('DOWNLOAD',download_tab)]])]]
 
 window = sg.Window('Downtube', layout)
 
